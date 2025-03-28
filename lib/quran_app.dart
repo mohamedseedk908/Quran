e4d6e229c7cd1c1caed053138130582f475cc7b3
Mohamed Seedyk <mohamedseedk908@gmail.com>
2025-03-29 00:39:41 +0200

    create project

---
+++ lib/quran_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class QuranApp extends StatelessWidget {
+  const QuranApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
